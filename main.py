e3cd4bfcb19fa231bea42214a8da295d8b0feeaa
ArthurProenca <arthur.rodriguesproenca@gmail.com>
2021-09-13 11:45:48 -0300

    oi

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import requests
 
-def main():
-    buscar_dados()
-
 def buscar_dados():
     request = requests.get("https://apisidra.ibge.gov.br/values/t/1612/n6/3101607")
     print(request.content)
+
+buscar_dados()
